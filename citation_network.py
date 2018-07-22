b4d0f07244fc24f68e96d7812646ea8a0d94a7f0
Lasse Nordahl <lassenordahl@Lasses-MacBook-Pro.local>
2018-07-21 18:58:19 -0700

    plotly network example now fixed for all compilation errors. Still has issues with creating edges that needs to be debugged.

--- citation_network.py
+++ citation_network.py
@@ -2,7 +2,7 @@ import PyPDF2
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
-import networkx
+import networkx as nx
 import os
 import csv 
 
@@ -15,6 +15,8 @@ NODE_CSV_NAME = 'smaller_sample_nodes.csv'
 
 def main():
 
+	testNetworkGraph()
+
 	research_node = create_nodes_from_csv(CSV_DIRECTORY + NODE_CSV_NAME)
 
 	for filename in os.listdir(PDF_DIRECTORY):
@@ -22,7 +24,7 @@ def main():
 			# print_pdf_contents(PDF_DIRECTORY, filename)
 			map_citation_network(research_node, PDF_DIRECTORY, filename)
 
-	print_research_node(research_node)
+	# print_research_node(research_node)
 
 
 def print_pdf_contents(directory_name, filename):
@@ -88,9 +90,92 @@ def print_research_node(research_node):
 		print(key, ' references ')
 		print(value['references'])
 		print()
-		# for key_two, value_two in value['references']:
-		# 	print(key_two)
-		# print()
+
+
+def testNetworkGraph():
+	G=nx.random_geometric_graph(200,0.125)
+	pos=nx.get_node_attributes(G,'pos')
+
+	dmin=1
+	ncenter=0
+	for n in pos:
+		x,y=pos[n]
+		d=(x-0.5)**2+(y-0.5)**2
+		if d<dmin:
+			ncenter=n
+			dmin=d
+
+	p=nx.single_source_shortest_path_length(G,ncenter)
+
+	edge_trace = go.Scatter(
+    x=[],
+    y=[],
+    line=dict(width=0.5,color='#888'),
+    hoverinfo='none',
+    mode='lines')
+
+	for edge in G.edges():
+		x0, y0 = G.node[edge[0]]['pos']
+		x1, y1 = G.node[edge[1]]['pos']
+		# print(x0, y0)
+		edge_trace['x'] = [x0, x1, None]
+		edge_trace['y'] = [y0, y1, None]
+
+	node_trace = go.Scatter(
+		x=[],
+		y=[],
+		text=[],
+		mode='markers',
+		hoverinfo='text',
+		marker=dict(
+			showscale=True,
+			# colorscale options
+			# 'Greys' | 'Greens' | 'Bluered' | 'Hot' | 'Picnic' | 'Portland' |
+			# Jet' | 'RdBu' | 'Blackbody' | 'Earth' | 'Electric' | 'YIOrRd' | 'YIGnBu'
+			colorscale='Greys',
+			reversescale=True,
+			color=[],
+			size=10,
+			colorbar=dict(
+				thickness=15,
+				title='Node Connections',
+				xanchor='left',
+				titleside='right'
+			),
+			line=dict(width=2)))
+
+	for node in G.nodes():
+		x, y = G.node[node]['pos']
+		# print(x, y)
+		# node_trace['x'].append(x)
+		# node_trace['y'].append(y)
+		node_trace['x'] += (x,)
+		node_trace['y'] += (y,)
+
+	for node, adjacencies in enumerate(G.adjacency()):
+		# node_trace['marker']['color'].append(len(adjacencies))
+		node_trace['marker']['color'] += (len(adjacencies), )
+		node_info = '# of connections: '+str(len(adjacencies))
+		# node_trace['text'].append(node_info)
+		node_trace['text'] = (node_info, )
+
+
+	fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='<br>Network graph made with Python',
+                titlefont=dict(size=16),
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Test Graph haha",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
+
+	py.plot(fig, filename='networkx')
 
 
 if __name__ == "__main__":
